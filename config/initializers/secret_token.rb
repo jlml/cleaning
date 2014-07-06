25b5b9898ddda8822382ab7d6333774370bace63
Yitao Zhang <Yitao@Yitaos-MacBook-Air.local>
2014-07-06 15:36:28 +0800

    clean site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CleanerSiteFinal::Application.config.secret_key_base = 'c243ea7833f0ae5481b6d53ae8dd333010d9e4be4f6377540f6166ba9d3e40528ceef4712ce537ce3adb0f564f577ee6fdbcd235ab1bfcb0cdf46f252da35b32'
