ef834eeed47d5751999d55ec177c6cf6d74b9b39
Yitao Zhang <Yitao@Yitaos-MacBook-Air.local>
2014-07-06 15:44:57 +0800

    blank landing page

--- Gemfile
+++ Gemfile
@@ -30,7 +30,6 @@ gem 'jbuilder', '~> 1.2'
 gem 'wicked'	
 
 gem 'bootstrap-sass', '~> 3.2.0'
-gem 'sass-rails', '>= 3.2'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CleanerSiteFinal::Application.routes.draw do
+  get "site/landing"
+
+  root :to => "site#landing"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
