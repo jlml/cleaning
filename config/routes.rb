99be541d329d3c328c0e845d3156f94b5ed185ee
Yitao Zhang <Yitao@Yitaos-MacBook-Air.local>
2014-07-12 14:19:12 +0800

    pre wicked with right order table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CleanerSiteFinal::Application.routes.draw do
   get "site/landing"
 
+  resources :order
+
   root :to => "site#landing"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ db/migrate/20140712060908_create_orders.rb
@@ -0,0 +1,20 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :location
+      t.string :cleaner
+      t.date :cleandate
+      t.time :cleantime
+      t.integer :rooms
+      t.integer :bathroom
+      t.time :cleanduration
+      t.float :price
+      t.integer :number
+      t.string :email
+      t.string :name
+      t.string :message
+
+      t.timestamps
+    end
+  end
+end
